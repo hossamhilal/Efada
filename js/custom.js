b966dd9134d598620510097e09527d7c183790aa
Hossam Hilal <46751584+hossamhilal@users.noreply.github.com>
2021-01-02 21:36:17 +0300

    Header Fixed When Scroll

--- js/custom.js
+++ js/custom.js
@@ -26,6 +26,11 @@
         $('body').removeClass('no-scroll');
     });
 
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > 100) $('header').addClass('fixedTop');
+        else $('header').removeClass('fixedTop');
+    });
+
 
     // Check if Rtl 
     var rtlVal = true ;   
